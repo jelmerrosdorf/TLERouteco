b06eac87924becf55e632ef28df56a1806ae42c5
thelucky_owl <1014147@hr.nl>
2022-04-06 11:52:42 +0200

    Test commit

--- App/App.js
+++ App/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test commit fela branch</Text>
       <StatusBar style="auto" />
     </View>
   );
