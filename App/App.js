4df9a118604bb73651ea33e645abe093e17d14fa
Jelmer Rosdorf <jelmerr99@gmail.com>
2022-04-06 12:35:00 +0200

    Test 2

--- App/App.js
+++ App/App.js
@@ -4,11 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
-      <Text>Test voor Jelmer's branch!</Text>
-=======
       <Text>Test Caspar</Text>
->>>>>>> 0e8183f825f9af468324f9bfb4a923efa19eda08
       <StatusBar style="auto" />
     </View>
   );
