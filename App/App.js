8bc45914b7de1f7e4e1abd0bfe3087ea238aa426
Jelmer Rosdorf <jelmerr99@gmail.com>
2022-04-06 12:41:49 +0200

    test commit folders

--- App/App.js
+++ App/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test Caspar</Text>
+      <Text>Test branch Jelmer</Text>
       <StatusBar style="auto" />
     </View>
   );
