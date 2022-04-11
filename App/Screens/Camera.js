c1da533c6a9d9edf2a2501c5c964312ba0e43266
thelucky_owl <1014147@hr.nl>
2022-04-11 05:48:30 +0200

    added nav button

---
+++ App/Screens/Camera.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+export const Camera = ({navigation, route}) =>{
+    return(
+        <View>
+
+<TouchableOpacity
+                onPress={()=>{
+                    navigation.navigate('Gallery')
+                }}
+                style={{
+                    height:50,
+                    width:'70%',
+                    alignSelf: "center",
+                    backgroundColor:"green",
+                    justifyContent:"center"
+                }}
+                >
+                <Text
+                style={{
+                    alignSelf:"center",
+                    color:"white"
+                }}
+                >
+                    go to gallery
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+
+
+}
