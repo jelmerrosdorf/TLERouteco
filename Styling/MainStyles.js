f21aa9a00772011161e2dfdb4b38c95beacf967d
Jelmer Rosdorf <jelmerr99@gmail.com>
2022-04-11 21:20:41 +0200

    Test commit welcomescreen

---
+++ Styling/MainStyles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native-web";
+
+export const mainStyles = StyleSheet.create({
+    page: {
+        flex: 1,
+        backgroundColor: '#4be373',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        padding: 25,
+        fontSize: 35,
+    },
+    logo: {
+        width: 500,
+        height: 600,  
+    },
+  });
