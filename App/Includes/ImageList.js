fa9630fe306c64c1f12a8e33cf98899fec593184
thelucky_owl <1014147@hr.nl>
2022-04-11 20:57:14 +0200

    List of image sources

---
+++ App/Includes/ImageList.js
@@ -0,0 +1,19 @@
+const images = [
+
+    {
+        id:1,
+        source: require("../Includes/Images/leaf1.jpeg")
+    },
+    {
+        id:2,
+        source: require("../Includes/Images/leaf2.jpg")
+    },
+    {
+        id:3,
+        source: require("../Includes/Images/leaf3.jpg")
+    }
+
+
+
+]
+module.exports = images;
