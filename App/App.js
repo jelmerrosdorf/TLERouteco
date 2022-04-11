0da87e859d5332364eeef4397ebed7b830e47c90
thelucky_owl <1014147@hr.nl>
2022-04-11 05:48:06 +0200

    separated screens and app.js, and add stack nav

--- App/App.js
+++ App/App.js
@@ -1,12 +1,52 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'; 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Camera } from './Screens/Camera';
+import { Gallery } from './Screens/Gallery';
+import { Image1 } from './Screens/Image1';
+import { Image2 } from './Screens/Image2';
+import { Image3 } from './Screens/Image3';
+import { Image4 } from './Screens/Image4';
+
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Test branch main</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+      initialRouteName='Gallery'
+      >
+        <Stack.Screen
+        name='Camera'
+        component = {Camera}
+        />
+        <Stack.Screen
+        name='Gallery'
+        component = {Gallery}
+        />
+        <Stack.Screen
+        name='Image1'
+        component = {Image1}
+        />
+        <Stack.Screen
+        name='Image2'
+        component = {Image2}
+        />
+        <Stack.Screen
+        name='Image3'
+        component = {Image3}
+        />
+        <Stack.Screen
+        name='Image4'
+        component = {Image4}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+   
   );
 }
 
