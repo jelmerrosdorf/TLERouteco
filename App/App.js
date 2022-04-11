79681e7a71d5f2428bc41c48a58fcdbc8c7e9f51
Gerri-Eickhof <knakkerf@users.noreply.github.com>
2022-04-11 19:43:50 +0200

    Merge branch 'main' of https://github.com/jelmerrosdorf/TLERouteco into Luc

--- App/App.js
+++ App/App.js
@@ -1,14 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import EditAccount from './Screens/editAccountScreen';
+import Login from './Screens/loginScreen';
+import Register from './Screens/registerScreen';
 
+const Stack = createNativeStackNavigator();
+
+// App shows the end product for user
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Test branch main</Text>
-      <StatusBar style="auto" />
-    </View>
+  
+  return(
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Register'
+      >
+
+        { /* Loginscreen for the temporary code */ }
+        <Stack.Screen 
+          name="Login"
+          component={Login}
+          options={ {
+            title: "Login",
+            headerShown: false,
+          } }
+        />
+
+        { /* Homescreen, in which it shows all the categories of a certain questionnaire */ }
+        <Stack.Screen 
+          name="Register"
+          component={Register}
+        />
+
+        {/* each induvidual category and the questions that belong to that category */}
+        <Stack.Screen 
+          name="EditAccount" 
+          component={EditAccount} 
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
