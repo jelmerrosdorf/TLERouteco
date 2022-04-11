ad77b58961981c3278afb08e9bc43802155bc8a1
thelucky_owl <1014147@hr.nl>
2022-04-11 23:41:18 +0200

    deleted hardcoded image screens

--- App/App.js
+++ App/App.js
@@ -6,10 +6,6 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { Camera } from './Screens/Camera';
 import { Gallery } from './Screens/Gallery';
-import { Image1 } from './Screens/Image1';
-import { Image2 } from './Screens/Image2';
-import { Image3 } from './Screens/Image3';
-import { Image4 } from './Screens/Image4';
 import {ViewImage} from './Screens/ViewImage';
 
 const Stack = createStackNavigator();
@@ -27,22 +23,6 @@ export default function App() {
         <Stack.Screen
         name='Gallery'
         component = {Gallery}
-        />
-        <Stack.Screen
-        name='Image1'
-        component = {Image1}
-        />
-        <Stack.Screen
-        name='Image2'
-        component = {Image2}
-        />
-        <Stack.Screen
-        name='Image3'
-        component = {Image3}
-        />
-        <Stack.Screen
-        name='Image4'
-        component = {Image4}
         />
          <Stack.Screen
         name='ViewImage'
