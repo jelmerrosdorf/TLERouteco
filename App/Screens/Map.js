0855bfac551173f03d429a266f9c83911803a29f
thelucky_owl <1014147@hr.nl>
2022-05-18 14:51:33 +0200

    user location & coordinates are shown

--- App/Screens/Map.js
+++ App/Screens/Map.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect }from 'react';
 import { View, Text, TouchableOpacity, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 import { MainStyles } from '../Styles/MainStyles';
 
-const Map = ({navigation, route}) =>{  return (
-        
+const Map = ({navigation, route}) =>{  
+    const [location, setlocation] = useState(null);
+    const[errorMsg, setErrorMsg] = useState(null);
+    useEffect(()=>{
+        (async()=>{
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if(status !== 'granted'){
+                setErrorMsg('Permission was denied :(')
+                return
+            }
+            let location = await Location.getCurrentPositionAsync({})
+            setlocation(location)
+        })();
+    }, []);
 
-
+    let longitudeText
+    let latitudeText
+    if(errorMsg){
+        text = errorMsg
+    }else if(location){
+        longitudeText = (JSON.stringify(location.coords.longitude))
+        latitudeText = (JSON.stringify(location.coords.latitude))
+    }
+    
+    return (
 
     <View style={MainStyles.mapContainer}>
             <MapView style={{
                 width: Dimensions.get('window').width,
                 height: Dimensions.get('window').height}}
                 initialRegion={{
-                    latitude: 51.91736,
-                    longitude: 4.48466,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421
+                    latitude: 52.00882,
+                    longitude:  4.36038,
+                    latitudeDelta: 0.0722,
+                    longitudeDelta: 0.0221
                   }}
+                  showsUserLocation
                 >
                 <Marker coordinate={{
                     latitude: 51.91736,
@@ -27,6 +50,22 @@ const Map = ({navigation, route}) =>{  return (
                 description={"desc"}
                 />
             </MapView>
+                    <TouchableOpacity
+                onPress={()=>{
+                  navigation.navigate('Login')
+                }}
+                style={MainStyles.mapButton}
+                >
+                <Text
+                style={MainStyles.buttonText}
+                >
+                    u coordinaten zijn:{"\n"}
+                    longitude :{longitudeText}{"\n"}
+                    latitude :{latitudeText}
+                    
+                </Text>
+            </TouchableOpacity>
+                
 
 
     </View>
