22fbbc632af14e5b940f08f8ed3d5c3f7edd1bf3
thelucky_owl <1014147@hr.nl>
2022-05-31 12:55:07 +0200

    created function to get and show current cords

--- App/Screens/Map.js
+++ App/Screens/Map.js
@@ -1,12 +1,19 @@
 import React, {useState, useEffect }from 'react';
 import { View, Text, TouchableOpacity, Dimensions } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Polyline } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { MainStyles } from '../Styles/MainStyles';
 
-const Map = ({navigation, route}) =>{  
-    const [location, setlocation] = useState(null);
+
+const Map = ({navigation, route}) =>{
+    const [location, setLocation] = useState(null);
+    const [longText, setLongText] = useState(null);
+    const [latText, setLatText] = useState(null);
     const[errorMsg, setErrorMsg] = useState(null);
+    let longitudeInt = 0
+    let latitudeInt = 0
+
+    
     useEffect(()=>{
         (async()=>{
             let {status} = await Location.requestForegroundPermissionsAsync();
@@ -15,7 +22,8 @@ const Map = ({navigation, route}) =>{
                 return
             }
             let location = await Location.getCurrentPositionAsync({})
-            setlocation(location)
+            setLocation(location)
+            
         })();
     }, []);
 
@@ -24,10 +32,18 @@ const Map = ({navigation, route}) =>{
     if(errorMsg){
         text = errorMsg
     }else if(location){
-        longitudeText = (JSON.stringify(location.coords.longitude))
-        latitudeText = (JSON.stringify(location.coords.latitude))
+        //update marker coords, rerenders marker on map
+        latitudeInt = location.coords.latitude
+        longitudeInt = location.coords.longitude
+        console.log("hello")
     }
     
+    function GetCords(){
+         Location.getCurrentPositionAsync({accuracy: Location.Accuracy.Highest,
+            maximumAge: 10000,
+            timeout: 5000})
+            .then(res => setLocation(res))
+        }
     return (
 
     <View style={MainStyles.mapContainer}>
@@ -35,41 +51,42 @@ const Map = ({navigation, route}) =>{
                 width: Dimensions.get('window').width,
                 height: Dimensions.get('window').height}}
                 initialRegion={{
-                    latitude: 52.00882,
-                    longitude:  4.36038,
-                    latitudeDelta: 0.0722,
-                    longitudeDelta: 0.0221
+                    latitude: 51.91742,
+                    longitude: 4.48501,
+                    latitudeDelta: 0.1922,
+                    longitudeDelta: 0.1421
                   }}
-                  showsUserLocation
+                  mapType='hybrid'
+                showsUserLocation
                 >
                 <Marker coordinate={{
-                    latitude: 51.91736,
-                    longitude: 4.48466,
+                    latitude: latitudeInt,
+                    longitude: longitudeInt,
                 }}
                 title={"Hello"}
                 description={"desc"}
                 />
+                
             </MapView>
                     <TouchableOpacity
                 onPress={()=>{
-                  navigation.navigate('Login')
+                  GetCords()
                 }}
                 style={MainStyles.mapButton}
                 >
                 <Text
                 style={MainStyles.buttonText}
                 >
-                    u coordinaten zijn:{"\n"}
-                    longitude :{longitudeText}{"\n"}
-                    latitude :{latitudeText}
+                    U coordinaten zijn:{"\n"}
+                    longitude :{location.coords.longitude}{"\n"}
+                    latitude :{location.coords.latitude}
                     
                 </Text>
             </TouchableOpacity>
-                
-
-
+            
     </View>
   );
+  
 }
 
 export default Map;
